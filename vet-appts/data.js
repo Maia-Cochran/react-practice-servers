47dbfda4ca98aae001d8ef5e289343e1466652ef
Maia Cochran (she/they) <101746747+Maia-Cochran@users.noreply.github.com>
2022-09-14 11:31:42 -0700

    Update data.js

--- vet-appts/data.js
+++ vet-appts/data.js
@@ -1,4 +1,4 @@
 const appointments = [{id: 1, pet: 'Spot', date: 'April 23', time: '1:00'},
-                     ];
+                     {id: 2, pet: 'Something', date: '', time: 'Another'}];
 
 module.exports = appointments;
